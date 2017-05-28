ad1b5daa44028726b47b32f39e9b4e5db7f247f8
Edith Mandujano <edithmandujano001@gmail.com>
2017-05-28 03:27:32 -0500

    Carga archivo,solo muestra la URL

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 var mensajeTitulo = document.getElementById("mensajeTitulo");
 var mensajeIngresado = document.getElementById("mensajeIngresado");
 var botonPublicarMensaje = document.getElementById("mensajePublicar");
-var mensajesPublicados = document.getElementById("mensajesPublicados");
+var mensajesPublicados = document.getElementById("publicados");
+var botonPublicarImagen=document.getElementById("publicarImagen");
+var imagenTitulo=document.getElementById("imagenTitulo");
+var imagen=document.getElementById("imagenArchivo");
 
 botonPublicarMensaje.addEventListener("click", publicarMensaje);
+botonPublicarImagen.addEventListener("click", validaCargaDeArchivos);
 
 function publicarMensaje() {
   var titulo = document.createElement("h4");
   var tituloIngresar = document.createTextNode(mensajeTitulo.value);
   var mensaje = document.createElement("p");
   var mensajeIngresar = document.createTextNode(mensajeIngresado.value);
-  
+
   titulo.appendChild(tituloIngresar);
   mensajesPublicados.appendChild(titulo);
   titulo.style.color="pink";
@@ -18,4 +22,30 @@ function publicarMensaje() {
   mensaje.appendChild(mensajeIngresar);
   mensajesPublicados.appendChild(mensaje);
   mensaje.style.color="grey";
+}
+
+/*carga de archivos*/
+function validaCargaDeArchivos(){
+  /*la condicional verifica si tu navegador acepta la carga de archivos*/
+  if (window.File && window.FileReader && window.FileList && window.Blob) {
+    ingresarImagen();
+  }
+
+  else {
+    alert('Tu navegador NO soporta la carga de archivos');
+  }
+}
+
+
+function ingresarImagen(){
+  var titulo = document.createElement("h4");
+  var tituloIngresar = document.createTextNode(imagenTitulo.value);
+  var imagenContenedor = document.createElement("div");
+  var imagenParaIngresar=imagen.value;
+  titulo.appendChild(tituloIngresar);
+  mensajesPublicados.appendChild(titulo);
+  titulo.style.color="pink";
+
+  imagenContenedor.innerHTML=imagenParaIngresar;
+  mensajesPublicados.appendChild(imagenContenedor);
 }
